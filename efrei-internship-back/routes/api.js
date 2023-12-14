578b0ab8e6d6b0126e6521684b30579f714e104d
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2023-12-14 16:22:03 +0100

    Initialization of the back and the database

---
+++ efrei-internship-back/routes/api.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const data_db = require('./.data_db');
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  "db_efrei_internship",
+  data_db.Username,
+  data_db.Password,
+  {
+    dialect: "postgres",
+    host: data_db.Host,
+    port: data_db.Port,
+  }
+);
+
+try {
+  sequelize.authenticate().then(() => {
+    console.log("Connection to MySQL database has been established successfully.");
+  });
+} catch (error) {
+  console.error("Unable to connect", error);
+}
+
+router.use((req, res, next) => {
+  next();
+});
+
+module.exports = router;
---
+++ efrei-internship-bdd/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE db_efrei_internship;
+
+CREATE USER ei_user WITH PASSWORD 'ei_password';
+GRANT ALL PRIVILEGES ON DATABASE db_efrei_internship TO ei_user;
