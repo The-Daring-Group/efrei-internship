93dd7cfb54ab4a37e03886044180ae1c1a097460
alachaud <antoine.lachaud91620@gmail.com>
2024-01-17 11:24:05 +0100

    Routing now working

---
+++ efrei-internship-front/pages/about.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <h1>About page</h1>
+    </div>
+</template>
---
+++ efrei-internship-front/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><NuxtLink to="about">About</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <h1>Home Page - Child component below</h1>
+        <ButtonCounter/>
+    </div>
+</template>
