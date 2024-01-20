6ccb7fa1c264725dcc6fcbb7d98ed98288219473
alachaud <antoine.lachaud91620@gmail.com>
2024-01-20 16:21:09 +0100

    Added FontAwesome

--- efrei-internship-front-ta/components/HeaderTA.vue
+++ efrei-internship-front-ta/components/HeaderTA.vue
@@ -4,7 +4,7 @@
         <nav>
             <ul class="text-white bg-teal-500 p-1 rounded-md flex flex-row justify-between">
                 <li class="hover:bg-teal-800 bg-teal-600 w-fit p-2 rounded-md"><NuxtLink to="presentation/list">Presentations</NuxtLink></li>
-                <li class="hover:bg-teal-200 bg-teal-100 text-teal-700 w-fit p-2 rounded-md">Disconnect</li>
+                <li class="hover:bg-teal-200 bg-teal-100 text-teal-700 w-fit p-2 rounded-md">Disconnect <font-awesome-icon class="ml-0.5" icon="fa-solid fa-arrow-right-from-bracket" /></li>
             </ul>
         </nav>
     </div>
--- efrei-internship-front-ta/nuxt.config.ts
+++ efrei-internship-front-ta/nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
