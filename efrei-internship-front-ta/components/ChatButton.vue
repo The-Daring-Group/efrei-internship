1a7197d17c9bb945ac0dc154ef26b9401890d1dc
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2024-01-23 19:11:00 +0100

    Create a chat button
    start a chat page

---
+++ efrei-internship-front-ta/components/ChatButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <NuxtLink :to="'/internships/chat/' + userId" class="tw-relative tw-bg-blue-500 hover:tw-bg-blue-600 text-white font-bold px-2 rounded tw-w-11 tw-h-8">
+        <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.5 3.5H3.5a2 2 0 00-2 2V18.5a2 2 0 002 2h6.07l1.75 1.75a1 1 0 001.42 0l2.33-2.33H20.5a2 2 0 002-2V5.5a2 2 0 00-2-2zM12 14l-2 2m0 0l-4-4m4 4l4-4"/>
+        </svg>
+        <div v-if="counter > 0" class="tw-absolute tw-top-0 tw-right-1 tw-w-4 tw-h-4 tw-text-xs tw-text-white tw-bg-red-500 tw-rounded-full tw-flex tw-items-center tw-justify-center">
+            {{ counter < 10 ? counter : '9+'}}
+        </div>
+    </NuxtLink>
+</template>
+
+<script>
+    // <button @click="increment"
+    export default {
+        /*props: {
+            userId: {
+                type: Number,
+                required: true
+            }
+        },*/
+        data() {
+            return {
+                counter: 0,
+                userId: 1
+            }
+        },
+        methods: {
+            increment() {
+                //console.log('increment')
+                this.counter++
+            }
+        }
+    }
+</script>
---
+++ efrei-internship-front-ta/pages/internships/chat/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Chat with User {{ userId }}</h1>
+      <!-- Ajoutez ici votre interface de chat, messages, etc. -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        userId: null
+      };
+    },
+    async fetch() {
+      const userId = parseInt(this.$route.params.id, 10);
+      console.log(userId);
+      this.userId = userId;
+    }
+  };
+  </script>
+  
