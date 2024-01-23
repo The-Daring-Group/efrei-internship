10dc813d91c24d0f9d7ed0716f79deef3338750f
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2024-01-23 13:50:12 +0100

    Add queries chat

--- efrei-internship-back/services/ei-service-chat/routes/api.js
+++ efrei-internship-back/services/ei-service-chat/routes/api.js
@@ -16,30 +16,6 @@ const sequelize = new Sequelize(
   }
 );
 
-const Chat = sequelize.define("chat", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  id_sender: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  id_receiver: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-});
-
 try {
   sequelize.authenticate().then(() => {
     console.log("Connection to PostgreSQL database has been established successfully.");
@@ -47,8 +23,6 @@ try {
 } catch (error) {
   console.error("Unable to connect", error);
 }
-
-sequelize.sync();
 
 router.use((req, res, next) => {
   next();
@@ -58,15 +32,43 @@ router.get("/", (req, res) => {
   res.send("Welcome to the chat API");
 });
 
-router.get("/messages", (req, res) => {
-  /*sequelize
+router.get("/messages", (req, res) => { // Get all messages
+  sequelize
     .query("SELECT * FROM chat", { type: Sequelize.QueryTypes.SELECT })
     .then((messages) => {
       res.json(messages);
-    });*/
-  Chat.findAll().then((messages) => {
-    res.json(messages);
-  });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/messages/:userId1/:userId2", (req, res) => { // Get messages between two users
+  sequelize
+    .query("SELECT * FROM chat WHERE (id_sender = ? AND id_receiver = ?) OR (id_sender = ? AND id_receiver = ?)", {
+      replacements: [req.params.userId1, req.params.userId2, req.params.userId2, req.params.userId1],
+      type: Sequelize.QueryTypes.SELECT,
+    })
+    .then((messages) => {
+      res.json(messages);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/messages", (req, res) => { // Post a message
+  sequelize
+    .query("INSERT INTO chat (user_id_1, user_id_2, message) VALUES (?, ?, ?)", {
+      replacements: [req.body.user_id_1, req.body.user_id_2, req.body.message],
+      type: Sequelize.QueryTypes.INSERT,
+    })
+    .then((messages) => {
+      res.json(messages);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
