cc49ac4b7afea8893ec97c597414f515e0dfef5e
alachaud <antoine.lachaud91620@gmail.com>
2024-01-25 15:16:31 +0100

    Reworked API to use nodemon and .env file

--- efrei-internship-back/services/ei-service-internship-info/app.js
+++ efrei-internship-back/services/ei-service-internship-info/app.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const session = require('express-session')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const session = require('express-session');
+const logger = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+const { Sequelize } = require('sequelize');
 
 const apiRouter = require('./routes/api.js')
 
@@ -9,10 +11,40 @@ const app = express()
 
 app.use(cors({ origin: 'http://localhost:8001' }))
 app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: 'demo secret', saveUninitialized: true, resave: true }))
+
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PORT = process.env.DB_PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+
+// Sequelize setup
+const sequelize = new Sequelize(
+    `${DB_NAME}`,
+    `${DB_USERNAME}`,
+    `${DB_PASSWORD}`,
+    {
+        dialect: 'postgres',
+        host: `${DB_HOST}`,
+        port: `${DB_PORT}`,
+    }
+);
+
+// Check database connection
+sequelize.authenticate()
+    .then(() => {
+      console.log('Database reached and connected!');
+    })
+    .catch(err => {
+      console.error('Unable to connect to database: ', err);
+    });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/', apiRouter)
 
-module.exports = app
+app.listen('3002', () => {
+    console.log(`Now listening on port 3002`)
+})
