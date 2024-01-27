962accc56213f12d209309e36d2fc65e21419f3f
mustache956 <antoine.rincheval@gmail.com>
2024-01-27 11:25:22 +0100

    creation of the connection between front and service, service and firebase
    - upload fonction added

---
+++ efrei-internship-back/ei-service-ftp_firebase/routes/fileRoutes.js
@@ -0,0 +1,14 @@
+// src/routes/fileRoutes.js
+const express = require('express');
+const fileController = require('../controllers/fileController');
+// const filecontrService = require('../services/service');
+const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+module.exports = function(app) {
+    app.post("/upload_files", upload.single("file"), fileController.uploadFile);
+};
+
