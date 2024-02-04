0e09218ac1c53995b7b655e39a12e3361ef007c5
adancingmilk <antoine.lachaud91620@gmail.com>
2024-02-04 13:47:52 +0100

    TA - Disconnect now working

--- efrei-internship-front-ta/components/HeaderTA.vue
+++ efrei-internship-front-ta/components/HeaderTA.vue
@@ -13,27 +13,22 @@
                 <li class="hover:tw-bg-cyan-800 tw-bg-cyan-600 tw-w-fit tw-p-2"><NuxtLink to="/internships/list">Internships</NuxtLink></li>
             </ul>
             <ul class="tw-flex tw-flex-row">
-                <li class="hover:tw-bg-sky-800 tw-bg-sky-700 tw-text-sky-200 tw-w-fit tw-p-2 tw-mr-2" v-if="!isConnected"><NuxtLink to="../login">Login</NuxtLink></li>
-                <li class="hover:tw-bg-sky-800 tw-bg-sky-700 tw-text-sky-200 tw-w-fit tw-p-2 tw-mr-2" v-else>Disconnect <font-awesome-icon class="tw-ml-0.5" :icon="['fas', 'arrow-right-from-bracket']" /></li>
+                <li class="hover:tw-bg-sky-800 tw-bg-sky-700 tw-text-sky-200 tw-w-fit tw-p-2 tw-mr-2 hover:tw-cursor-pointer" @click="logout">Disconnect <font-awesome-icon class="tw-ml-0.5" :icon="['fas', 'arrow-right-from-bracket']" /></li>
             </ul>
         </nav>
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            isConnected: false
-        }
-    },
-    // mounted() {
-    //     // this.isConnected = this.$store.state.isConnected
-    // },
-    // watch: {
-    //     isConnected() {
-    //         this.isConnected = this.$store.state.isConnected
-    //     }
-    // },
+<script setup lang="ts">
+import { useSessionStore } from '#imports';
+const sessionStore = useSessionStore();
+const userInfos = ref(sessionStore.getUser)
+const logout = () => {
+  useFetch('http://localhost:3000/api/logout', {
+    method: 'POST',
+  }).then(() => {
+    sessionStore.logout()
+    navigateTo('/login')
+  })
 }
 </script>
