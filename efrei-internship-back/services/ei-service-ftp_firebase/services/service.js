39b91bd18b233293ff1b2ac65a794e8bf4e15851
mustache956 <antoine.rincheval@gmail.com>
2024-02-05 09:03:58 +0100

    correction of last commit missing function

--- efrei-internship-back/services/ei-service-ftp_firebase/services/service.js
+++ efrei-internship-back/services/ei-service-ftp_firebase/services/service.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { initializeApp } = require('firebase/app');
 const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
 const firebaseConfig = require('../utils/firebaseConfig');
+const {sequelize, QueryTypes} = require('../utils/initSequelize.js');
 
 const router = express.Router();
 
@@ -14,22 +15,61 @@ const storage = getStorage();
 //TODO: use a more complex identifier for the file name
 async function uploadFile(req) {
 
-        const storageRef = ref(storage, req.body.name  + '_' + req.file.originalname);
+    const { id_student, id_academic_tutor, id_company_tutor, type, name } = req.body;
 
-        // Create file metadata including the content type
-        const metadata = {
-            contentType: req.file.mimetype,
-        };
+    const storageRef = ref(storage, name  + '_' + req.file.originalname);
 
-        // Upload the file in the bucket storage
-        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer , metadata);
-        //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
+    // Create file metadata including the content type
+    const metadata = {
+        contentType: req.file.mimetype,
+    };
 
-        // Grab the public url
-        const downloadURL = await getDownloadURL(snapshot.ref);
+    // Upload the file in the bucket storage
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer , metadata);
+    //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
 
-        console.log('File successfully uploaded.');
+    // Grab the public url
+    const downloadURL = await getDownloadURL(snapshot.ref);
+
+    SaveDocURL(downloadURL, id_student, id_academic_tutor, id_company_tutor, type, name);
+
+    console.log('File successfully uploaded.');
 };
+
+async function SaveDocURL(url, id_student, type, name) {
+    try {
+        const id_academic_tutor = await sequelize.query(
+            `SELECT id_academic_tutor FROM internship WHERE id_student = :id_student`,
+            {
+                replacements: {id_student},
+                type: QueryTypes.SELECT,
+            });
+
+        const id_company_tutor = await sequelize.query(
+            `SELECT id_company_tutor FROM internship WHERE id_student = :id_student`,
+            {
+                replacements: {id_student},
+                type: QueryTypes.SELECT,
+            });
+
+
+        await sequelize.query(
+            `INSERT INTO document (url, validated_by_company, validated_by_school, id_student, id_academic_tutor, id_company_tutor, type, name) VALUES (?, FALSE, FALSE, ?, ?, ?, ?, ?)`,
+            {
+                replacements: [url, id_student, id_academic_tutor, id_company_tutor, type, name],
+                type: QueryTypes.INSERT,
+            }
+        );
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+async function GetFileURL(req) {
+    
+    return downloadURL;
+}
 
 
 module.exports = {
